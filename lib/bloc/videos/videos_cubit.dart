7ca0f01f4b5c60ce091e3f5348e56c3bc15c9230
Aayush Regmi <aayush.181509@ncit.edu.np>
2022-01-02 19:00:12 +0545

    added constant file with apikey and Dio().get broken down to url and query param

--- lib/bloc/videos/videos_cubit.dart
+++ lib/bloc/videos/videos_cubit.dart
@@ -3,18 +3,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+import 'package:final_app/constant/string_constants.dart';
 import 'package:final_app/models/video_model/video_data.dart';
 import 'package:final_app/models/video_model/video_hit.dart';
+import 'package:flutter/cupertino.dart';
 part 'videos_state.dart';
 
 class VideosCubit extends Cubit<VideosState> {
   VideosCubit() : super(VideosInitial());
   getVideosFromServer() async {
     emit(VideosLoading(message: 'Loading'));
-    final url =
-        "https://pixabay.com/api/videos/?key=24747073-90cabfd95266df4486476df9e&q=yellow+flowers&pretty=true";
+    final url = "https://pixabay.com/api/videos";
     try {
-      final result = await Dio().get(url);
+      final result = await Dio().get(url, queryParameters: {
+        'key': APIKEY,
+        'page': 1,
+        'per_page': 5,
+      });
       print('This is our data from server');
       print(result.statusCode);
       print(result.data);
